7e7e7df353453aaf365816802676af2a4ec2c37c
diankhismaw <diankhismaw@gmail.com>
2025-11-08 18:30:15 +0700

    feat: change layout selectSeat studio page

--- src/lib/mockFilmData.ts
+++ src/lib/mockFilmData.ts
@@ -9,5 +9,5 @@ export const mockFilmData: { [key: string | number]: FilmStudioInfo } = {
   2: { film_name: "The Avengers", theater_name: "CGV", poster_url: "/poster/avengers.jpeg" },
   3: { film_name: "Joker", theater_name: "Cinepolis", poster_url: "/poster/joker.webp" },
   4: { film_name: "Interstellar", theater_name: "Cinema XXI", poster_url: "/poster/interstellar.jpeg" },
-  5: { film_name: "Inception", theater_name: "CGV", poster_url: "/poster/now-you-see-me.jpg" },
+  5: { film_name: "Now You See Me", theater_name: "CGV", poster_url: "/poster/now-you-see-me.jpg" },
 };
